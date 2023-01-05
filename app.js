7b1a16e136c242f8c336957bfdf7b85bfe8a1260
Hanna <hanna-marii.kriisa@voco.ee>
2023-01-05 16:47:05 +0200

    let & const

---
+++ app.js
@@ -0,0 +1,34 @@
+// length (mitu elementi arrays on)
+const numbers = [1,2,3]
+
+console.log(numbers.length)
+
+// For Each Loop
+// Variant function sõnaga
+numbers.forEach(function(number){
+    console.log(number)
+})
+
+// Arrow function (teeb sama, mis eelmine, aga ilma function sõnata)
+numbers.forEach((number) => {
+    console.log(number)
+})
+
+// Objekt
+const person = {
+    name: 'Kertu',
+    age : 25
+}
+
+// Toob objekti väärtused eraldi
+console.log(person)
+console.log(person.name)
+console.log(person.age)
+
+// Funktsioon cons'ina
+const multiply = (number) => {
+    return number * number
+}
+
+// Numbrile antakse väärtus 2
+console.log(multiply(2))
